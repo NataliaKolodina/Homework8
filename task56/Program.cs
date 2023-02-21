76ae2b58a79c7efba9045881c28998e40231c3ed
«Natalia <kolodina_melbyk@mail.ru>
2023-02-21 14:14:58 +0400

    add task56

--- task56/Program.cs
+++ task56/Program.cs
@@ -12,7 +12,7 @@ int rows = int.Parse(Console.ReadLine()!)!;
 Console.WriteLine("Введите количество столбцов массива: ");
 int colums = int.Parse(Console.ReadLine()!)!;
 
-int[,] array = GetArray(rows, colums, 1, 3);
+int[,] array = GetArray(rows, colums, 1, 10);
 PrintArray(array);
 Console.WriteLine();
 int[] array1 = SumElemRows(array);
