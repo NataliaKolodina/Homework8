f8ac3343a94a0ba0fd29e2a92c16fc22a5ee1862
«Natalia <kolodina_melbyk@mail.ru>
2023-02-21 19:36:15 +0400

    add task58

--- task58/Program.cs
+++ task58/Program.cs
@@ -12,13 +12,13 @@ int rows = int.Parse(Console.ReadLine()!)!;
 Console.WriteLine("Введите количество столбцов массивов: ");
 int colums = int.Parse(Console.ReadLine()!)!;
 
-int[,] array1 = GetArray(rows, colums, -10, 10);
+int[,] array1 = GetArray(rows, colums, 0, 10);
 PrintArray(array1);
 Console.WriteLine();
-int[,] array2 = GetArray(colums, rows, -10, 10);
+int[,] array2 = GetArray(colums, rows, 0, 10);
 PrintArray(array2);
 Console.WriteLine();
-int[,] arrayRes = MatrixMultiplication(array1, array2)
+int[,] arrayRes = MatrixMultiplication(array1, array2);
 PrintArray(arrayRes);
 
 int[,] GetArray(int m, int n, int minValue, int maxValue)
@@ -46,14 +46,20 @@ void PrintArray(int[,] inArray)
     }
 }
 
-int[,] MatrixMultiplication(int[] arr1, int[] arr2)
+int[,] MatrixMultiplication(int[,] arr1, int[,] arr2)
 {
-    int[,] res = new int[arr1.GetLength(0), arr1.GetArray(1)];
+    int[,] res = new int[arr1.GetLength(0), arr2.GetLength(1)];
     for (int i = 0; i < arr1.GetLength(0); i++)
     {
-        for (int j = 0; j < arr1.GetArray(1); j++)
+        for (int j = 0; j < arr2.GetLength(1); j++)
         {
-            res[i, j] = arr1[i, j] * arr2[j, i];
+            int sum = 0;
+            for (int k = 0; k < arr1.GetLength(0); k++)
+            {
+                int a = (arr1[i, k] * arr2[k, j]);
+                sum = sum + a;
+            }
+            res[i, j] = sum;
         }
     }
     return res;
